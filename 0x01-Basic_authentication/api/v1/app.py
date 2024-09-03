7861294a6933ed9e033c31e80cc304d4d490d9b2
sayv-xo <98850746+sayv-xo@users.noreply.github.com>
2024-09-03 10:33:12 +0000

    fix error for 403

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -32,7 +32,7 @@ def unauthorized(error) -> str:
 def forbidden(error) -> str:
     """Forbidden error handler
     """
-    return jsonify({"error": "Forbidden"})
+    return jsonify({"error": "Forbidden"}), 403
 
 
 if __name__ == "__main__":
