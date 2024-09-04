2e07ee0f2b7573f9db54215147e00b448c1a45a6
sayv-xo <98850746+sayv-xo@users.noreply.github.com>
2024-09-04 05:58:34 +0000

    basic authentication

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -41,7 +41,9 @@ class Auth:
         Returns:
             None
         """
-        return None
+        if request is None or 'Authorization' not in request.headers:
+            return None
+        return request.headers.get('Authorization')
 
     def current_user(self, request=None) -> TypeVar('User'):
         """Defines the current user
---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""Basic authentication module"""
+from .auth import Auth
+
+
+class BasicAuth(Auth):
+    """Basic authentication class
+    """
+    pass
