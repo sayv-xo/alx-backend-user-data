9e0fef671cce2839e9841395343fb8c73dbd1ae8
sayv-xo <98850746+sayv-xo@users.noreply.github.com>
2024-09-06 05:10:12 +0000

    create dict for sessions and id

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -2,8 +2,22 @@
 """Session authentication class
 """
 from .auth import Auth
+import uuid
 
 
 class SessionAuth(Auth):
     """Session authentication class"""
-    pass
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """Create a session id for a user_id
+        Args:
+            user_id: id to create session for
+        Returns:
+            enerated session id or None if user_id is None or not a string
+        """
+        if type(user_id) is not str or user_id is None:
+            return None
+        session_id = uuid.uuid4()
+        self.user_id_by_session_id[str(session_id)] = user_id
+        return str(session_id)
